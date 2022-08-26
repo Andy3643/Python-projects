54cf1e3d690ac197e00d16a53c16077da1f2f3f1
@Andy <joshua.mulonga@student.moringaschool.com>
2022-08-26 09:57:17 +0300

    ft-pop up canvas

--- weather program/openWeather API/weather.py
+++ weather program/openWeather API/weather.py
@@ -1,12 +1,14 @@
 #create weather app using API
+import json
 from multiprocessing import Condition
+from pickle import GET
 import tkinter as tk
 import time
 from tkinter import font
 import requests
 
 #function to get JSON 
-def getweather():
+def getweather(canvas):
     cityname = textfield.get()
     apicall = "https://api.openweathermap.org/data/2.5/weather?q=" +cityname+ "&appid=1a39fad784dc2505bd8df8c31af4491c"
     json_data = requests.get(apicall).json() #get api data
@@ -16,7 +18,15 @@ def getweather():
     max_temp = int(json_data['main']['temp_max'] - 273.15)
     pressure = json_data['main']['pressure']
     humidity = json_data['main']['humidity']
+    wind = json_data['wind']['speed']
+    sunrise = time.strftime("%H:%M:%S",time.gmtime(json_data['sys']['sunrise']))
+    sunset = time.strftime("%H:%M:%S",time.gmtime(json_data['sys']['sunset']))
 
+    final_info = condition + "\n" + str(temperature) + " degrees"
+    final_data = "\n" + "Max Temp: " + str(max_temp) + "\n" + "Min temp: " + str(min_temp) + "Pressure" + str(pressure) + "\n" + "Humidity" + str(humidity) + "\n" + "Wind speed: " + str(wind) + "\n" + "Sunrise: " + sunrise + "Sunset: " + sunset 
+
+    label1.config(text = final_info)
+    label2.config(text = final_data)
 
 #create GUI canvas
 canvas = tk.Tk()
@@ -28,12 +38,15 @@ font1 = ("poppins",15, "bold")
 font2 = ("poppins",35, "bold")
 
 #create user entry field for the search
-textfield = tk.Entry(canvas,font = font2)
+textfield = tk.Entry(canvas,font = font2, justify = 'center')
 textfield.pack(pady=20)
 textfield.focus()
+textfield.bind('<Return>',getweather)
 
 #creating labels to show data
 label1 = tk.Label(canvas, font = font2)
 label1.pack()
 label2 = tk.Label(canvas, font = font1)
 label2.pack()
+ 
+canvas.mainloop()
